92fda8277ea9cb7e2d05b827f009f9ae7bd8e240
feigebaobei <18515195415@163.com>
2022-09-07 21:34:51 +0800

    docs(crtp-cli): init rollup.config.js

---
+++ packages/crtp-cli/assets/rollup.config.js
@@ -0,0 +1,17 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+export default [
+  {
+    input: ['index.js'],
+    output: {
+      dir: 'dist',
+      // entryFileNames: [name].js,
+      assetFileNames: [name]-[hash][extend],
+      chunkFileNames: [name]-[hash].js,
+      format: 'esm',
+      compact: false
+    },
+    external: [],
+    plugins: [nodeResolve(), commonjs()]
+  }
+]
